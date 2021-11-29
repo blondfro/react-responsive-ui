b9f2784a049673e27258a5d9f8e219fa74c9c77e
James Morris <blondfro@gmail.com>
2021-11-28 18:14:55 -0600

    added the basic set of files and folders needed.

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./possibility.css";
+
+function Possibility() {
+    return (
+        <div>
+            Possibilities
+        </div>
+    )
+}
+
+export default Possibility;
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./whatGPT3.css";
+
+function WhatGpt3() {
+    return (
+        <div>
+            What...
+        </div>
+    )
+}
+
+export default WhatGpt3;
