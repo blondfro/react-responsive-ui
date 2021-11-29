409aeecd6887ada9c66acc2c4cff9c85c6296640
James Morris <blondfro@gmail.com>
2021-11-28 18:25:09 -0600

    imported in the basic layout to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Header, Footer, Features, Blog, Possibility, WhatGPT3 } from "./containers";
+import { Navbar, Brand, CTA } from "./components";
 
 function App() {
     return (
-        <div>
-            
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
