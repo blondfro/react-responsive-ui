4bfa838aad5cea6ff96f5b3962072e701938ae1e
James Morris <blondfro@gmail.com>
2021-12-04 17:32:38 -0600

    implemented the cta component.

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,18 @@ import './cta.css';
 
 function CTA() {
     return (
-        <div>
-            Call to Action
+        <div className="gpt3__cta">
+            <div className="gpt3__cta-content">
+                <p>
+                    Request Early Access to Get Started
+                </p>
+                <h3>
+                    Register today & start exploring the endless possibilities.
+                </h3>
+            </div>
+            <div className="gpt3__cta-btn">
+                <button type="button">Get Started</button>
+            </div>
         </div>
     )
 }
