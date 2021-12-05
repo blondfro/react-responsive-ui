413b97696c3ac500385b6f37b829a6fb0f278b7f
James Morris <blondfro@gmail.com>
2021-12-04 22:09:46 -0600

    implemented the footer components.  also updated the navbar component to have the href.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Menu() {
         <>
             <p><a href="#home">Home</a></p>
             <p><a href="#wgpt3">What is GPT3?</a></p>
-            <p><a href="#posisibility">Open AI</a></p>
+            <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
         </>
